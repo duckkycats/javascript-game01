172759aa8621e5cf6b29374ac78d4ecb92dde796
duckkycats <earth_of_joke@hotmail.com>
2023-08-01 10:11:28 +0700

    create Topic classes

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', function(){
+    // canvas setup
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projecttile {
+
+    }
+    
+    class Particle {
+
+    }
+
+    class Player {
+
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    // display something to user
+    class UI {
+
+    }
+
+    
+    class Game {
+
+    }
+
+
+
+});
