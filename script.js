2477943cde1f6ac6099856088f4dc32d15d24e39
duckkycats <earth_of_joke@hotmail.com>
2023-11-19 16:44:02 +0700

    add sound effect

--- script.js
+++ script.js
@@ -27,6 +27,41 @@ window.addEventListener('load', function(){
         }
     }
 
+    class SoundController {
+        constructor() {
+            this.powerUpSound = document.getElementById('powerup');
+            this.powerDownSound = document.getElementById('powerdown');
+            this.explosionSound = document.getElementById('explosion');
+            this.shotSound = document.getElementById('shot');
+            this.hitSound = document.getElementById('hit');
+            this.shieldSound = document.getElementById('shield');
+        }
+        powerUp(){
+            this.powerUpSound.currentTime = 0;
+            this.powerUpSound.play();
+        }
+        powerDown(){
+            this.powerDownSound.currentTime = 0;
+            this.powerDownSound.play();
+        }
+        explosion(){
+            this.explosionSound.currentTime = 0;
+            this.explosionSound.play();
+        }
+        shot(){
+            this.shotSound.currentTime = 0;
+            this.shotSound.play();
+        }
+        hit(){
+            this.hitSound.currentTime = 0;
+            this.hitSound.play();
+        }
+        shield(){
+            this.shieldSound.currentTime = 0;
+            this.shieldSound.play();
+        }
+    }
+
     class Projectile {
         constructor(game, x, y) {
             this.game = game;
@@ -135,6 +170,7 @@ window.addEventListener('load', function(){
                     this.powerUpTimer = 0;
                     this.powerUp = false;
                     this.frameY = 0;
+                    this.game.sound.powerDown();
                 } else {
                     this.powerUpTimer += deltaTime;
                     this.frameY = 1;
@@ -157,6 +193,7 @@ window.addEventListener('load', function(){
                 this.projectiles.push(new Projectile(this.game, this.x + 80, this.y + 30));
                 this.game.ammo--;
             }
+            this.game.sound.shot();
             if (this.powerUp) this.shootBottom();
         }
 
@@ -171,6 +208,7 @@ window.addEventListener('load', function(){
             this.powerUpTimer = 0;
             this.powerUp = true;
             if (this.game.ammo < this.game.maxAmmo) this.game.ammo = this.game.maxAmmo;
+            this.game.sound.powerUp();
         }
     }    
 
@@ -444,6 +482,7 @@ window.addEventListener('load', function(){
             this.player = new Player(this);
             this.input = new InputHandler(this);
             this.ui = new UI(this);
+            this.sound = new SoundController();
             this.keys = [];
             this.enemies = [];
             this.particles = [];
@@ -482,7 +521,8 @@ window.addEventListener('load', function(){
                 enemy.update();
                 if (this.checkCollision(this.player, enemy)) {
                     enemy.markedForDeletion = true;
-                    this.addExplosion(enemy)
+                    this.addExplosion(enemy);
+                    this.sound.hit();
                     for (let i = 0; i < enemy.score; i++){
                         this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5));
                     }
@@ -500,6 +540,7 @@ window.addEventListener('load', function(){
                             }
                             enemy.markedForDeletion = true;
                             this.addExplosion(enemy);
+                            this.sound.explosion();
                             if (enemy.type === 'moon') this.player.enterPowerUp();
                             if (enemy.type === 'hive'){
                                 for (let i = 0; i < 5; i++){
